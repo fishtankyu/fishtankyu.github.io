d0f8521890bb336e9900aa6cbe2b9edbb6acfcf4
ZENG YU <83162708+fishtankyu@users.noreply.github.com>
2025-04-26 12:31:14 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',       // serve at repo root
-});
+  base: '/',           // your site is at the repo root
+  build: {
+    outDir: 'docs'     // <-- tell Vite to write into `docs/` instead of `dist/`
+  }
+})
