a378e193cb3daa12aa448007b43fc30ce70413ba
ZENG YU <83162708+fishtankyu@users.noreply.github.com>
2025-04-26 12:27:07 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"] ,
-  darkMode: 'class',
-  theme: { extend: {} },
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
